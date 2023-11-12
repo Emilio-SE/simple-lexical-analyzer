444f94404509badad944d7a210bcbb30fbfbb60e
Emilio SE <emiliose.g@hotmail.com>
2023-11-12 13:35:35 -0600

    Init java project

---
+++ src/main/java/org/lexicalAnalizer/Main.java
@@ -0,0 +1,12 @@
+package org.lexicalAnalizer;
+
+/**
+ * Author: Emilio Sánchez Enriquez
+ * Date: 12/11/2023
+ * Project: Analizador Léxico
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
